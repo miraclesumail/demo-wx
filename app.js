cec27315f63516e56648bc9c6d4939de5d73eda2
miraclesumail <857247710@qq.com>
2019-03-22 17:59:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -187,3 +187,150 @@ document.addEventListener("message", function(event) {
 
 http://bastengao.com/blog/2017/08/webview-with-local-html-in-react-native.html
 
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    LayoutAnimation, UIManager, Clipboard
+  } from 'react-native';
+ 
+
+    var CustomLayoutAnimation = {
+      duration: 500,
+      create: {
+        type: LayoutAnimation.Types.linear,
+        property: LayoutAnimation.Properties.scaleXY,
+      },
+      update: {
+        type: LayoutAnimation.Types.linear,
+      }
+    };
+  
+export default class AnimationExample extends Component {
+    state = {
+        index: 0,
+    }
+  
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     index: 0,
+    //   }
+    // }
+
+    componentWillMount(){
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);  
+        Clipboard.setString('wtfddd');   
+    }
+
+    componentDidMount(){
+        Clipboard.getString().then(res => console.log(res));
+    }
+  
+    onPress(index) {
+  
+      // Uncomment to animate the next state change.
+      //LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+  
+      // Or use a Custom Layout Animation
+       LayoutAnimation.configureNext(CustomLayoutAnimation);
+  
+      this.setState({index: index});
+    }
+  
+    renderButton(index) {
+      return (
+        <TouchableOpacity key={'button' + index} style={styles.button} onPress={() => this.onPress(index)}>
+          <Text>{index}</Text>
+        </TouchableOpacity>
+      );
+    }
+  
+    renderCircle(key) {
+      var size = 50;
+      return (
+        <View key={key} style={{width: size, height: size, borderRadius: size / 2.0, backgroundColor: 'sandybrown', margin: 20}}/>
+      );
+    }
+  
+    render() {
+      var leftStyle = this.state.index === 0 ? {flex: 1} : {width: 20};
+      var middleStyle = this.state.index === 2 ? {width: 20} : {flex: 1};
+      var rightStyle = {flex: 1};
+  
+      var whiteHeight = this.state.index * 80;
+  
+      var circles = [];
+      for (var i = 0; i < (5 + this.state.index); i++) {
+        circles.push(this.renderCircle(i));
+      }
+  
+      return (
+        <View style={styles.container}>
+          <View style={styles.topButtons}>
+            {this.renderButton(0)}
+            {this.renderButton(1)}
+            {this.renderButton(2)}
+          </View>
+          <View style={styles.content}>
+            <View style={{flexDirection: 'row', height: 100}}>
+              <View style={[leftStyle, {backgroundColor: 'firebrick'}]}/>
+              <View style={[middleStyle, {backgroundColor: 'seagreen'}]}/>
+              <View style={[rightStyle, {backgroundColor: 'steelblue'}]}/>
+            </View>
+            <View style={{height: whiteHeight, justifyContent: 'center', alignItems: 'center', overflow: 'hidden'}} removeClippedSubviews={true}>
+              <View>
+                <Text>Stuff Goes Here</Text>
+              </View>
+            </View>
+            <View style={styles.circleContainer}>
+              {circles}
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    topButtons: {
+      marginTop: 22,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width:300,
+      //alignSelf: 'stretch',
+      backgroundColor: 'lightblue',
+    },
+    button: {
+      flex: 1,
+      height: 60,
+      alignSelf: 'stretch',
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 8,
+    },
+    content: {
+      flex: 1,
+      alignSelf: 'stretch',
+    },
+    circleContainer: {
+      flexDirection: 'row',
+      flex: 1,
+      flexWrap: 'wrap',
+      padding: 30,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+  });
